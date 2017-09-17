4ea4bfae74bcfd3614e238e00d7be0242e016c0e
Zhitao <zw56@njit.edu>
2017-09-16 21:33:07 -0400

    update AWS ip

--- service/main.go
+++ service/main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://34.223.243.88:9200"
+	ES_URL = "http://35.162.183.37:9200"
 )
 
 
