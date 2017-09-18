53364478488a66b7847e03b7c9b6417d12c8cb7d
Zhitao <zw56@njit.edu>
2017-09-18 11:42:48 -0400

    update main.go

--- service/main.go
+++ service/main.go
@@ -33,7 +33,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.162.183.37:9200"
+	ES_URL = "http://34.210.216.146:9200"
 	PROJECT_ID ="totemic-winter-180122"
 	BT_INSTANCE="around-post"
 )
